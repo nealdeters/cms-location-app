0d4c7f1410cbfd89b2ad30597a9d319476e5cdc1
nealdeters <nealdeters@gmail.com>
2016-03-02 19:21:42 -0600

    removed aws-ses gem and initializer file

--- config/application.rb
+++ config/application.rb
@@ -32,21 +32,10 @@ module CmsLocationApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    # config.action_mailer.delivery_method = :smtp
-    # # SMTP settings for gmail
-    # config.action_mailer.smtp_settings = {
-    #  :address              => "smtp.gmail.com",
-    #  :port                 => 587,
-    #  :domain               => "gmail.com",
-    #  :user_name            => ENV['GMAIL_USERNAME'],
-    #  :password             => ENV['GMAIL_PASSWORD'],
-    #  :authentication       => :plain,
-    # :enable_starttls_auto => true
-    # }
 
     # # config.action_mailer.delivery_method = :sendmail
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
+    # config.action_mailer.perform_deliveries = true
+    # config.action_mailer.raise_delivery_errors = true
 
     
     config.active_record.raise_in_transactional_callbacks = true
