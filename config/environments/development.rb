818b5f263f15c6d7950391ab85d922388f9f3169
nealdeters <nealdeters@gmail.com>
2016-03-07 19:33:34 -0600

    fixed pe-icon-7-stroke font route

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+  # Precompile additional assets
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
