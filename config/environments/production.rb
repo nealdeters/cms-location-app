8257e39750dc97a1f44f01021576edf076e4b3ed
nealdeters <nealdeters@gmail.com>
2016-03-11 12:25:06 -0600

    setup heroku to allow masked redirects

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #Heroku to allow masked redirects to my app
+  config.action_dispatch.default_headers = { 'X-Frame-Options' => 'ALLOWALL' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
