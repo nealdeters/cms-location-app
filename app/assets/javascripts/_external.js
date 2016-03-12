c47ecd9645283671d4b73c0a671b33f8b781b9d5
nealdeters <nealdeters@gmail.com>
2016-03-12 01:22:33 -0600

    added tooltip to hours and image category. added favicon customization to images and webpage

---
+++ app/assets/javascripts/_external.js
@@ -0,0 +1,16 @@
+$(document).ready (function(){
+
+  $('[data-toggle=popover]').popover({
+      content: $('#myPopoverContent').html(),
+      html: true
+  });
+  
+  $(".alert-dismissible").fadeTo(2000, 500).slideUp(500, function(){
+      $(".alert-dismissible").alert('close');
+  });  
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  }) 
+  
+});
