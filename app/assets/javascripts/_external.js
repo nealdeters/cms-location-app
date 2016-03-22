f47f11b22be4859e755caad10f9c7c9f672d73dc
nealdeters <nealdeters@gmail.com>
2016-03-22 18:42:22 -0500

    removed flexslider from external and applicaiton.js

--- app/assets/javascripts/_external.js
+++ app/assets/javascripts/_external.js
@@ -13,9 +13,9 @@ $(document).ready (function(){
     $('[data-toggle="tooltip"]').tooltip()
   }) 
 
-  $(window).load(function() {
-    $('.flexslider').flexslider();
-  });
+  // $(window).load(function() {
+  //   $('.flexslider').flexslider();
+  // });
 
   $(function() {
     var loc = window.location.href; // returns the full URL
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
-//= require theme/jquery.flexslider
 //= require angular
 //= require angular-animate
 //= require_tree .
